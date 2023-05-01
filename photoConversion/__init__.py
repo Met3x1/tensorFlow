1d0da932618ef3236ef8012fdfe920237e3e1367
Michal Balint <michal.balint@kosickaakademia.sk>
2023-05-01 14:54:17 +0200

    added resized photos to 28x28, and first instance of working tensorflow

---
+++ photoConversion/__init__.py
@@ -0,0 +1,69 @@
+from PIL import Image
+import os
+import csv
+
+# Set the path to the directory containing the input images
+input_dir = "C:\\Users\\mbali\\Desktop\\school\\kasv\\programovanie" \
+            "\\had\\potholeDetectionTest\\photos\\fotky train CSV"
+
+# Set the path to the directory where you want to save the output images and annotations
+output_dir = "C:\\Users\\mbali\\Desktop\\school\\kasv\\programovanie\\" \
+             "had\\potholeDetectionTest\\photos\\train fotky output"
+
+# Set the path to the annotations file
+annotations_file = "C:\\Users\\mbali\\Desktop\\school\\kasv\\programovanie" \
+            "\\had\\potholeDetectionTest\\photos\\fotky train CSV\\_annotations.csv"
+
+# Loop through each file in the input directory
+for filename in os.listdir(input_dir):
+    # Check if the file is an image
+    if filename.endswith(".jpg") or filename.endswith(".jpeg") or filename.endswith(".png"):
+        # Open the input image
+        input_image = Image.open(os.path.join(input_dir, filename))
+
+        # Resize the image to 28x28
+        output_image = input_image.resize((28, 28))
+
+        # Load the annotations for the input image
+        annotations = []
+        with open(annotations_file, "r") as f:
+            reader = csv.reader(f)
+            for row in reader:
+                if row[0] == filename:
+                    annotations.append(row[1:]) # Append the annotations (excluding the filename)
+
+        # Scale the annotations proportionally to the new image size
+        scaled_annotations = []
+        for annotation in annotations:
+            coords = [int(coord) if coord.isdigit() else coord for coord in annotation]
+            if len(coords) != 4:
+                continue  # Skip this annotation if it doesn't have 4 coordinates
+            x1, y1, x2, y2 = coords
+            x1 = int(x1 * 28 / input_image.size[0])
+            y1 = int(y1 * 28 / input_image.size[1])
+            x2 = int(x2 * 28 / input_image.size[0])
+            y2 = int(y2 * 28 / input_image.size[1])
+            scaled_annotations.append((x1, y1, x2, y2))
+
+        # Save the output image
+        output_image.save(os.path.join(output_dir, filename))
+
+        # Save the annotations for the output image
+        with open(os.path.join(output_dir, filename + ".csv"), "w") as f:
+            writer = csv.writer(f)
+            for annotation in scaled_annotations:
+                writer.writerow(annotation)
+    # Check if the file is a text file
+    elif filename.endswith(".txt"):
+        # Open the input text file
+        with open(os.path.join(input_dir, filename), "r") as f:
+            text = f.read()
+
+        # Create a new output text file
+        with open(os.path.join(output_dir, filename), "w") as f:
+            # Write the text in uppercase to the output file
+            f.write(text.upper())
+
+    else:
+        # File is not an image or a text file, skip it
+        continue
---
+++ pokus.py
@@ -0,0 +1,49 @@
+import tensorflow as tf
+import matplotlib.pyplot as plt
+
+train_dir = "C:\\Users\\mbali\\Desktop\\school\\kasv\\programovanie" \
+            "\\had\\potholeDetectionTest\\photos\\train fotky output"
+test_dir = "C:\\Users\\mbali\\Desktop\\school\\kasv\\programovanie" \
+           "\\had\\potholeDetectionTest\\photos\\test fotky output"
+
+# Load the images from the directories and normalize their pixel values
+train_dataset = tf.keras.preprocessing.image_dataset_from_directory(
+    train_dir,
+    image_size=(28, 28),
+    batch_size=32
+)
+train_images, train_labels = next(iter(train_dataset))
+train_images = train_images / 255.0
+
+test_dataset = tf.keras.preprocessing.image_dataset_from_directory(
+    test_dir,
+    image_size=(28, 28),
+    batch_size=32
+)
+test_images, test_labels = next(iter(test_dataset))
+test_images = test_images / 255.0
+
+# Visualize the first 25 images in the training set
+plt.figure(figsize=(10,10))
+for i in range(25):
+    plt.subplot(5,5,i+1)
+    plt.xticks([])
+    plt.yticks([])
+    plt.grid(False)
+    plt.imshow(train_images[i], cmap=plt.cm.binary)
+plt.show()
+
+#setup modelu
+model = tf.keras.Sequential([
+    tf.keras.layers.experimental.preprocessing.Rescaling(1./255, input_shape=(28, 28, 3)),
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(128, activation='relu'),
+    tf.keras.layers.Dense(10)
+])
+
+
+model.compile(optimizer = 'adam',
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              metrics=['accuracy'])
+
+model.fit(train_images,train_labels, epochs=10)
